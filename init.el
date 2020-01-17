9ecb104305299949f8a7f50c2827ba585e7b9aee
Albin Kroon <albkro94@hotmail.com>
2020-01-17 13:27:16 +0100

    Some more things, like transparency

--- init.el
+++ init.el
@@ -2,6 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 ;;; Bootstrapping use-package
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+			 ("org"   . "https://orgmode.org/elpa/")))
+(package-initialize)
+
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -9,11 +17,6 @@
 (eval-when-compile
   (add-to-list 'load-path "~/.emacs.d/elpa/use-package-2.4/")
   (require 'use-package))
-
-(package-initialize)
-(setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("gnu" . "http://elpa.gnu.org/packages/")
-			 ("org"   . "https://orgmode.org/elpa/")))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
@@ -24,7 +27,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magit org-bullets-mode org-bullets flycheck yasnippet-snippets yasnippet hungry-delete which-key beacon expand-region company nyan-mode darkokai-theme use-package python))))
+    (rainbow-mode magit org-bullets-mode org-bullets flycheck yasnippet-snippets yasnippet hungry-delete which-key beacon expand-region company nyan-mode darkokai-theme use-package python))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
